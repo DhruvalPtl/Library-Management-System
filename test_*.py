0c1c518d8be4d01661ebde4a51fa30163bc25a68
Dhruval Patel <156530333+DhruvalPtl@users.noreply.github.com>
2024-08-01 08:58:37 +0530

    Update test_*.py

--- test_*.py
+++ test_*.py
@@ -1,17 +1,22 @@
-# test_calculator.py
-import pytest
+# test_calculator_unittest.py
+import unittest
 from calculator import add, subtract
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(-1, 1) == 0
-    assert add(-1, -1) == -2
+class TestCalculator(unittest.TestCase):
 
-def test_subtract():
-    assert subtract(2, 1) == 1
-    assert subtract(2, 0) == 2
-    assert subtract(2, -2) == 4
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
 
-def test_add_invalid_type():
-    with pytest.raises(TypeError):
-        add("one", "two")
+    def test_subtract(self):
+        self.assertEqual(subtract(2, 1), 1)
+        self.assertEqual(subtract(2, 0), 2)
+        self.assertEqual(subtract(2, -2), 4)
+
+    def test_add_invalid_type(self):
+        with self.assertRaises(TypeError):
+            add("one", "two")
+
+if __name__ == '__main__':
+    unittest.main()
